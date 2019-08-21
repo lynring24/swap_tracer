acd840a7ab8c7cb11e14c47f952c4e24ec768991
hyerin chung <lync2846@gmail.com>
2019-08-21 21:44:28 +0900

    code for mpas.sh

---
+++ trace_mpas.sh
@@ -0,0 +1,52 @@
+#!/bin/bash 
+# Run a command and track the swap 
+
+set -e 
+
+if [ $# -lt 1 ] && [ $# -gt 3 ]; then
+	echo "Usage : $0 [-m] [mem limit in MiB] <command>"
+	exit 1
+fi 
+
+exectime=$(date "+%Y %b %d %T")
+
+if [ $# -eq 1 ]; then 
+option=false
+limit=-1
+comm="$1"
+else 
+	if [ $1 = "-m" ]; then 
+		option=true
+		limit=$2
+		comm="$3"
+	else
+		option=false
+		limit=$1
+		comm="$2"
+	fi
+fi
+
+# there are memory limitation needed use lazybox
+# else run command 
+
+if [ $limit -ne -1 ]; then 
+	sudo sh lazybox/scripts/run_memcg_lim.sh $limit "$comm"
+else
+	eval $comm
+fi
+
+echo "trace down $comm"
+#IFS=' ' read -ra path <<< $comm
+#fname=${path[0]##*/}
+
+mkdir -p plot
+mkdir -p graph
+
+
+sudo awk  '/swptrace\(python\)/ {print $1FS$2FS$3FS$7FS$8$9 }' /var/log/messages > input_log
+
+if [ "$option" = true ]; then
+sudo python trace.py -m input_log "$exectime" "$comm" 
+else
+sudo python trace.py input_log "$exectime" "$comm"
+fi
