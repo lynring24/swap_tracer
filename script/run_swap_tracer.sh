82c6c62516b6ed5c0ce5b536bc7c10e4c7fffd3f
hyerin chung <lync2846@gmail.com>
2019-09-10 22:02:02 +0900

    re-organizing

---
+++ script/run_swap_tracer.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+# Run a command and track the swap 
+
+set -e 
+
+if [ $# -lt 1 ] && [ $# -gt 3 ]; then
+	echo "Usage : $0 [-m] [mem limit in MiB] <command>"
+	exit 1
+fi 
+
+exectime=$(LANG=en_us_88591; date +"%Y %b %d %T")
+
+if [ $# -eq 1 ]; then 
+option=false
+limit=-1
+comm="$1"
+else 
+	if [ $1 = "-m" ]; then 
+		option=true
+		limit=$2
+		comm="$3"
+	else
+		option=false
+		limit=$1
+		comm="$2"
+	fi
+fi
+
+# there are memory limitation needed use lazybox
+# else run command 
+
+if [ $limit -ne -1 ]; then 
+	sudo sh lazybox/scripts/run_memcg_lim.sh $limit "$comm"
+else
+	eval $comm
+fi
+
+echo "trace down $comm"
+
+mkdir -p ../plot
+mkdir -p ../graph
+
+if [ "$option" = true ]; then
+sudo python trace.py -m "$exectime" "$comm" 
+else
+sudo python trace.py "$exectime" "$comm"
+fi
