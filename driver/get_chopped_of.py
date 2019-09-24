4727dc401f303005440d6f5d21422971bcc402e2
hyerin chung <lync2846@gmail.com>
2019-09-24 21:05:09 +0900

    modified get_chopped_of.py by the page number and releasing output

--- driver/get_chopped_of.py
+++ driver/get_chopped_of.py
@@ -1,45 +1,54 @@
 import os
 import sys
 import re
+import math
 import traceback
 import json
+import shutil
+import time, calendar
+from datetime import datetime
 
-UNIT = 1024*1024*1024
 BLOCK = 1024
 PAGE_SIZE= 4096
 
+def check_and_flush(address):
+    global line_count, block_id, line_block
+    if abs(previousAt - address) > BLOCK * PAGE_SIZE * PAGE_SIZE * PAGE_SIZE or line_count > PAGE_SIZE:
+       filepath = "../log/"+filename+"/block_"+str(block_id)+".csv"
+       dump_file = open(filepath, 'w') 
+       dump_file.write(json.dumps(line_block))
+       line_block = []
+       block_id += 1
+       line_count = 0
+       print address
 
-# cut of the plot 
-def flush(block, timestamp):  
-    with open("../log/"+timestamp, 'w') as output
-         output.write(json.dump(block))
 
 if __name__ == "__main__" :
+   global previousAt
    previousAt  = 0
-   path = path.rstrip(os.sep)
    global filename
-   filename = os.path.basename(sys.argv[1])
+   filename = sys.argv[1].split("/")[-1].split(".")[0]
    
    try : 
-        count = 0
-	os.system('mkdir ../log/'+filename)
-
+        line_count = 0
+	block_id = 0
 	line_block = []
+	
+	os.system('sudo mkdir -p ../log/'+filename)
    	with open(sys.argv[1], "r") as csv:
       	     for line in csv:
- 		 pattern="(.+:.+:\d{2}\.\d{6}) (.+)"
+ 		 pattern="(\d+:\d{2}:\d{2}\.\d{6}) (.+)"
           	 matched= re.compile(pattern).search(line)
 
                  if matched is None:
-                    continue
-
-                 address = matched.group(2).strip()
-                 address=int(address)
- 		 if asb(previousAt - address) > 1024 * UNIT or count > BLOCK:
-		    flush (line_block, matched.group(1).strip())
-		    
+                    continue 
+                 address = int(matched.group(2).strip())
+		 check_and_flush(address)
 		 line_block.append(matched.group(1)+" "+str(int(address /PAGE_SIZE)))
 		 previousAt = address
-		 count+=1
-   except: 
-        traceback.print_exec()
+		 line_count+=1
+
+   except Exception as ex: 
+         print ex
+         if os.path.exists("../log/"+filename):
+	    shutil.rmtree("../log/"+filename, ignore_errors=True)
