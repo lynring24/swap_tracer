3769d70f4e94b2798a616d6cf8a66cd0593f00ca
hyerin chung <lync2846@gmail.com>
2019-09-25 21:41:34 +0900

    test cgroup w/ noise_cancel

--- driver/get_chopped_of.py
+++ driver/get_chopped_of.py
@@ -5,16 +5,18 @@ import math
 import traceback
 import json
 import shutil
-import time, calendar
-from datetime import datetime
+from pattern import *  
 
 BLOCK = 1024
 PAGE_SIZE= 4096
+DIGIT_THRESHOLD = 12
+
+
 
 def check_and_flush(address):
     global line_count, block_id, line_block
-    if abs(preDIGIT - address) > BLOCK * PAGE_SIZE * PAGE_SIZE * PAGE_SIZE:
-       filepath = "../log/"+filename+"/block_"+str(block_id)+".csv"
+    if abs(prevAt - address) > BLOCK * PAGE_SIZE * PAGE_SIZE * PAGE_SIZE:
+       filepath = "../log/"+FILENAME+"/block_"+str(block_id)+".csv"
        with open(filepath, 'w') as dump:
        	    for line in line_block:
        	        dump.write(line+"\n")
@@ -24,35 +26,46 @@ def check_and_flush(address):
 
 
 if __name__ == "__main__": 
-   global preDIGIT
-   preDIGIT  = 0
-   global filename
-   filename = sys.argv[1].split("/")[-1].split(".")[0]
+   global prevAt
+   prevAt  = 0
    
-   if len(sys.argv) < 1 or len(sys.argv) > 2:
-      print "usage : python get_chopped_of.py [FILE TO CHOP]"
+   if len(sys.argv) < 2 or len(sys.argv) > 4:
+      print "usage : python get_chopped_of.py --noise-cancel [FILE TO CHOP]"
       exit(1)
 
+   global FILENAME, NOISE_CANCEL
+
+   if len(sys.argv) is  2:
+      file = sys.argv[1]
+      NOISE_CANCEL = False
+   else :
+      file = sys.argv[2]
+      NOISE_CANCEL = True
+
+   FILENAME = file.split("/")[-1].split(".")[0] 
+ 
    try : 
         line_count = 0
 	block_id = 0
 	line_block = []
 	
-	os.system('sudo mkdir -p ../log/'+filename)
-   	with open(sys.argv[1], "r") as csv:
-      	     for line in csv:
- 		 pattern="(\d+:\d{2}:\d{2}\.\d{6}) (.+)"
-          	 matched= re.compile(pattern).search(line)
+        os.system('sudo mkdir -p ../log/'+FILENAME)
 
-                 if matched is None:
-                    continue 
-                 address = int(matched.group(2).strip())
-		 check_and_flush(address)
-		 line_block.append(matched.group(1)+" "+str(int(address /PAGE_SIZE)))
-		 preDIGIT = address
-		 line_count+=1
+   	with open(file, "r") as csv:
+      	     for line in csv:
+	         matched = is_valid_ms(line)
+	         if matched is not None:
+                    address = matched.group(2).strip()
+		
+		    if NOISE_CANCEL and DIGIT_THRESHOLD > len(address):
+		       continue
+                    address = int(address)
+		    check_and_flush(address)
+		    line_block.append(matched.group(1)+" "+str(int(address /PAGE_SIZE)))
+		    prevAt = address
+		    line_count+=1
 
    except Exception as ex: 
          print ex
-         if os.path.exists("../log/"+filename):
-	    shutil.rmtree("../log/"+filename, ignore_errors=True)
+         if os.path.exists("../log/"+FILENAME):
+	    shutil.rmtree("../log/"+FILENAME, ignore_errors=True)
