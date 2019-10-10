301fc1d48f3fbcc4addde0d5ae75a0acc71b41ad
hyerin chung <lync2846@gmail.com>
2019-10-10 18:21:59 +0900

    setup rsyslog dateformat

---
+++ driver/setup.sh
@@ -0,0 +1,11 @@
+CHECKTIMEFORMAT=$(cat /etc/rsyslog.conf | egrep ActionFileDefaultTemplate)
+
+echo ${CHECKTIMEFORMAT}
+
+if [[ $CHECKTIMEFORMAT == *"#"* ]]; then 
+   echo "rsyslog timestamping in RFC 3339 format"
+else 
+   echo "rsyslog timestamp traditional file format "
+   cp /etc/rsyslog.conf /etc/rsyslog.conf.default
+   cp ./rsyslog.conf.rfc3339 /etc/rsyslog.conf
+fi
