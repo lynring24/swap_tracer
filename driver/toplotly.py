234feadb05c81853b59c0bf8aa126c376e1fa1da
hyerin chung <lync2846@gmail.com>
2019-10-11 14:48:08 +0900

    plotly for basic

--- driver/toplotly.py
+++ driver/toplotly.py
@@ -1,13 +1,23 @@
 import plotly.graph_objects as go
 import pandas as pd 
 import sys
+import csv
 
-data = pd.read_csv(sys.argv[1], delimiter=' ')
-print data[0]
-#fig = go.Figure(data=go.Bar(y=[2, 3, 1]))
-#fig.write_html('first_figure.html', auto_open=True)
-#fig.add_trace(go.Scatter(x=data['sec'], y=data['pvn'],
-#              mode='markers',
-#              name='markers'))
+sec = []
+pvn = []
 
-#fig.show()
+with open(sys.argv[1]) as csvfile:
+     plots = csv.reader(csvfile, delimiter=',')
+     for row in plots:
+         sec.append(row[0])
+	 pvn.append(row[1])
+#data = pd.read_csv(sys.argv[1], sep=' ', names=['sec', 'pvn'], header=None )
+
+
+fig = go.Figure(data=go.Bar(y=[2, 3, 1]))
+fig.write_html('first_figure.html', auto_open=True)
+fig.add_trace(go.Scatter(x=sec, y=pvn,
+	      mode='markers',
+              name='markers'))
+
+fig.show()
