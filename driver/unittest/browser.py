6c51f535187c8c9b0b978edfebca639e7e823c49
hyerin chung <lync2846@gmail.com>
2020-01-08 22:00:18 +0900

    add new unittest code for flask

--- driver/unittest/browser.py
+++ driver/unittest/browser.py
@@ -7,9 +7,32 @@ from requests import get
 
 def config_input():
     if len(sys.argv) > 1: 
-       set_mem_limit(sys.argv[1])
-       set_command(sys.argv[2])
-  
+       NOTEXIST = -1
+       #print sys.argv[1:]
+       for arg in sys.argv[1:]:
+          if arg.find('--mem=') > NOTEXIST:
+      	     set_mem_limit(arg[arg.find('=')+1:])
+          elif arg.find('--cmd') > NOTEXIST:
+       	     set_command('"%s"'%arg[arg.find('=')+1:]) 
+          elif arg.find('--ip') > NOTEXIST:
+       	     set_ip(arg[arg.find('=')+1:])
+          elif arg.find('--port') > NOTEXIST:
+       	     set_port(arg[arg.find('=')+1:])
+          else:
+             print '[error] invalid option %s'%arg
+             print "usage : python $SWPTARCE/exec.py <--mem=Mib > <--cmd=\"COMMAND\"> <--ip=PUBLIC_IP> <--port=PORT_TO_USE>"
+             sys.exit(1)
+
+
+
+def check_option():
+    print "\n---------------------------------------------------------------"
+    print " * command        :%s "%get_command()
+    print " * mem lim(Mib)   :%s "%get_mem_limit()
+    print " * public IP      :%s "%get_ip()
+    print " * port           :%s "%str(get_port())
+    print "---------------------------------------------------------------\n"
+
 
 def exe_cmd():
     try:
@@ -20,7 +43,7 @@ def exe_cmd():
 	    print "rsyslog timestamp traditional file format "
 	    os.system('cp /etc/rsyslog.conf /etc/rsyslog.conf.default')
 	    os.system('cp ./rsyslog.conf.rfc3339 /etc/rsyslog.conf')
-
+        print 'mem :%s'%str(get_mem_limit())
 	exe_instr='sudo sh $SWPTRACE/exec_mem_lim.sh '+ str(get_mem_limit()) +' \"'+ get_command() + '\"'
 	print "\n$"+ exe_instr
 	eval_result = os.system(exe_instr)
@@ -52,7 +75,7 @@ def awk_log():
         os.system(instr)
 	print "\n$ "+instr+"\n"
 	date_pattern= '%Y-%m-%dT%H:%M:%S,%f'
-	parse_pattern = '(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\,\d{6})\+\d{4} swptrace\((.+)\): map (\(swpentry: \d+, uaddr: \d+\))'
+	parse_pattern = '(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\,\d{6})[\+-]\d{4} swptrace\((.+)\): map (\(swpentry: \d+, uaddr: \d+\))'
 	set_pattern('DATE', date_pattern)
 	set_pattern('LOG', parse_pattern)
     except BaseException as ex:
@@ -62,12 +85,25 @@ def awk_log():
 
 
 def run_flask():
-    os.environ['SWPTRACE_LOG'] = ''
-    os.environ['SWPTRACE_CMD'] = 'this is the test for swptrace board'
-    os.environ['FLASK_APP']='./app.py'
-    ip = get('https://api.ipify.org').text
-    os.system('cd $SWPTRACE ; flask run --host=0.0.0.0')
+    os.environ['SWPTRACE_LOG'] = '../demo/log/2020-01-08T03:07:31.958102'
+    os.environ['FLASK_APP']='app.py'
+    os.environ['SWPTRACE_CMD']='$ TEST DATA OF CFD'
+    #ip = get('https://api.ipify.org').text
+    try: 
+      result = os.system('cd $SWPTRACE ; flask run --host=%s --port=%s'%(get_ip(), get_port()))
+      if result != 0:
+         raise OSError
+    except OSError:
+      print "[warning] invalid IP or port try 0.0.0.0:5000"
+      os.system('cd $SWPTRACE ; flask run')
      
 
 if __name__ == '__main__':
+   set_up()
+   config_input()
+   check_option()
+   #exe_cmd()
+   #set_up_path()
+   #awk_log()
+   #extract()
    run_flask() 
