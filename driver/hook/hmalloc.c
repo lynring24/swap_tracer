86d7bc7cc6fb4fe558a93143e221066d7b4b5ff1
hyerin chung <lync2846@gmail.com>
2020-02-13 06:58:02 +0000

    print format

--- driver/hook/hmalloc.c
+++ driver/hook/hmalloc.c
@@ -9,17 +9,13 @@
 void getISOTime();
 void* hmalloc(const char * filen, const int line, const char * funcn, char * argn, size_t size) {
 	void * res = malloc (size);
-	printf("%s:%u:%s allocate %s in %p(%lu)\n", filen , line , funcn, argn, res, size);
-	getISOTime();
-	return res;
-}
-
-void getISOTime() {
 	struct timeval tv;
 	struct tm * localTime;
 	time_t t = time(NULL);
 
 	gettimeofday(&tv, NULL);
 	localTime = localtime(&t);
-	printf("local: %04d-%02d-%02d %02d:%02d:%02d.%06ld\n", localTime->tm_year + 1900, localTime->tm_mon + 1, localTime->tm_mday, localTime->tm_hour, localTime->tm_min, localTime->tm_sec, tv.tv_usec);
+	printf("%04d-%02d-%02dT%02d:%02d:%02d.%06ld::", localTime->tm_year + 1900, localTime->tm_mon + 1, localTime->tm_mday, localTime->tm_hour, localTime->tm_min, localTime->tm_sec, tv.tv_usec);
+	printf("%s:%u:%s() %s=%p(%lu)\n", filen , line , funcn, argn, res, size);
+	return res;
 }
