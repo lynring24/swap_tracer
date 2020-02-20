1a5cb5d16e1052e947c08e160deeb24cc3e58581
hyerin chung <lync2846@gmail.com>
2020-02-20 02:35:30 -0800

    [...] get proper benchmark

--- driver/hmalloc.c
+++ driver/hmalloc.c
@@ -15,8 +15,8 @@ void* hmalloc(const char * filen, const int line, const char * funcn, char * arg
 
 	gettimeofday(&tv, NULL);
 	localTime = localtime(&t);
-	//printf("%04d-%02d-%02dT%02d:%02d:%02d.%06ld::", localTime->tm_year + 1900, localTime->tm_mon + 1, localTime->tm_mday, localTime->tm_hour, localTime->tm_min, localTime->tm_sec, tv.tv_usec);
-	//printf("%s:%u:%s() %s=%p(%lu)\n", filen , line , funcn, argn, res, size);
+	printf("%04d-%02d-%02dT%02d:%02d:%02d.%06ld::", localTime->tm_year + 1900, localTime->tm_mon + 1, localTime->tm_mday, localTime->tm_hour, localTime->tm_min, localTime->tm_sec, tv.tv_usec);
+	printf("%s:%u:%s() %s=%p(%lu)\n", filen , line , funcn, argn, res, size);
         openlog("Logs", LOG_NDELAY, LOG_DAEMON);
 	syslog(LOG_DEBUG, "swptracer::%s:%u:%s() %s=%p(%lu)\n", filen , line , funcn, argn, res, size); 
         closelog();
