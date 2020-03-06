6be1c34b6b1b40d92140aa801917ffc215a3574e
hyerin chung <lync2846@gmail.com>
2020-03-06 13:51:59 +0900

    reduce TMI

--- driver/merge.py
+++ driver/merge.py
@@ -1,13 +1,11 @@
 from utility import *
 
 TIME=0
-FNAME=1
-LINE=2
-FUNC=3
-VAR=4
-TOP=5
-BTM=6
-VMA=1
+TOP=1
+BTM=2
+FNAME=3
+FUNC=4
+VAR=5
 
 
 def merge():
@@ -16,15 +14,15 @@ def merge():
          global page_table
          page_table = []
          for track in merge:
-             item = track.split(',')
-             if len(item) > 2 : 
+             item = [x.strip() for x in track.split(',')]
+             if len(item) > 3 : 
                 add_page_table(item)
              else:
                 classify_area(item)
     merge.close()
 
 def add_page_table(item):
-    dump = [item[TIME], item[FNAME], item[LINE], item[FUNC], item[VAR], item[TOP], item[BTM]] 
+    dump = [item[TIME], item[TOP], item[BTM], item[FNAME], item[FUNC], item[VAR]] 
 
     if dump in page_table:
         page_table.append(dump)
@@ -35,13 +33,16 @@ def classify_area(item):
        # run key and find the key that fits most
        output=open(get_path('total'), 'a+')
        page_table.sort(reverse=True)
-       print page_table[0][0], page_table[-1][0]
        item[1]=item[1][:-2]
-       for track in page_table:
-           if track[TOP] <= item[VMA] and  item[VMA] <= track[BTM] and track[TIME] <= item[TIME]: 
-              # remove endline
-              item.extend({track[FNAME], track[LINE], track[FUNC], track[VAR]})
-              break
+       print item
+       if page_table is not None:
+          for track in page_table:
+              if track[TOP] <= item[TOP] and  item[TOP] <= track[BTM] and track[TIME] <= item[TIME]: 
+                 # remove endline
+                 print track
+                 item.extend([track[FNAME], track[FUNC], track[VAR]])
+                 print item
+                 break
        
        line = ','.join(item) + "\n"
        output.write(line)
