5a126e2b2d7d432df2ed64bb43fa031c2f9278bb
hyerin chung <lync2846@gmail.com>
2020-03-10 16:26:58 +0900

    [...] create plot of kmenas

--- driver/model.py
+++ driver/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 from pandas import DataFrame
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import csv 
 from utility import *
@@ -20,9 +21,14 @@ def build():
             kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
             kmeans.fit(df)
             wcss.append(kmeans.inertia_)
-        print wcss
         index_min = min(xrange(len(wcss)), key=wcss.__getitem__)
-        print wcss[index_min]
 
-        kmeans = KMeans(n_clusters=index_min, init='k-means++', max_iter=300, n_init=10, random_state=0)
+        kmeans = KMeans(n_clusters=7,  max_iter=100, n_init=10, random_state=0).fit(df)
+        centroids = kmeans.cluster_centers_
+        print (centroids)
+
+
+	plt.scatter(df['time'], df['vas'], c=kmeans.labels_.astype(float), s=50, alpha=0.5)
+        plt.scatter(centroids[:,0], centroids[:, 1], c='red', s=50)
+        plt.show()
         
