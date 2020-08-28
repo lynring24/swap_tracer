5d6f4bf09b0276d8a6f6e72667cb755b14d9d34e
Hyerin Chung <lync2846@gmail.com>
2020-08-28 12:03:43 +0900

    add subplots

--- driver/plot.py
+++ driver/plot.py
@@ -28,41 +28,58 @@ class Tracker(object):
 def plot_out(dir_path, mean_time):
     print "$ generate plot png"
 
-    fig, axis = plt.subplots()
 
     rsyslog = pd.read_csv(dir_path+"/rsyslog.csv")
-    # TODO : Expand to Nanosecond Timestamp
-    # rsyslog['timestamp'] = rsyslog.index
-
-    # groups = rsyslog[rsyslog['mode']!='map'].groupby('mode')
-    groups = rsyslog.groupby('mode')
-
+    rsyslog = rsyslog.query('cmd=="linear" & mode=="handle_mm"')
+    max_range = len(str(rsyslog['timestamp'].max()))
+    bins = [pow(10, x) for x in range(0, max_range)]
+    rsyslog['label'] = pd.cut(x=rsyslog['timestamp'], bins=bins)
+  
     summary_str = "\n[Summary]\n"
-    # add allocation trace if needed
+    labels={'in':'swap-in','map':'memory map', 'fault':'page fault','out':'swap-out', 'writepage':'file I/O', 'handle_mm':'total page fault'}
+    colors={'in':'red', 'out':'blue', 'map':'green', 'fault':'purple', 'allocation':'pink', 'handle_mm':'pink' }
+    zorders={'fault':5, 'map':10, 'out':0, 'handle_mm':3}
+    
+    subranges=[]
+    idx  =0 
+    for name, group in rsyslog.groupby('label'):
+        if (len(group.index)) > 0:
+            subranges.append([max(group.timestamp.min(), 0)  , group.timestamp.max() + 10])
+            idx = idx +1
+            
+    fig, axes = plt.subplots(ncols=len(subranges), nrows=1, sharey=True)
+    fig.tight_layout()
 
-    # add rsyslogs to plot 
-    modes = ['out']
-    labels={'in':'swap-in','map':'memory map', 'fault':'page fault','out':'swap-out', 'writepage':'file I/O'}
-    colors={'in':'red', 'out':'blue', 'map':'green', 'fault':'purple', 'allocation':'pink' }
-    zorders={'fault':5, 'map':10, 'out':0}
+    total_axes = len(axes) 
 
-    for name, group in groups:
-	if name in modes:
-		axis.plot(group.timestamp, group.address, label=labels[name], c=colors[name], marker='o', linestyle='', ms=5, zorder=zorders[name])
-		summary_str = summary_str + "\n * memory {} # : {}".format(labels[name], len(group.index)) 
-    summary_str = summary_str + "\n * average existence time in memory (usec) : {}".format(mean_time)
+    print total_axes, idx
+
+    for idx in range(0, total_axes):
+        plt.subplots_adjust(hspace =0.2)
+        axes[idx].set_xlim(subranges[idx][0], subranges[idx][1])
+        if idx == 0: 
+            axes[idx].spines['right'].set_visible(False)
+        elif idx == total_axes-1:
+            axes[idx].spines['left'].set_visible(False)
+        else:
+            axes[idx].spines['left'].set_visible(False)
+            axes[idx].spines['right'].set_visible(False)
+        for name, group in rsyslog.groupby('mode'):
+            axes[idx].plot(group.timestamp, group.address, label=labels[name], c=colors[name], marker='o', linestyle=' ', ms=5, zorder=zorders[name])
+
+    summary = "\n [Summary] \n"
+    for name, group in rsyslog.groupby('mode'):
+        summary = summary + "\n memory {} # : {}".format(labels[name], len(group.index))
+    summary = summary + "\n * average existense time in memory (usec) : {}".format(mean_time)
 
 
-    axis.legend()
-    plt.text(0.05, pow(10, 6), summary_str)
+    plt.text(0.05, pow(10, 5), summary)
+    axes[int(total_axes/2)].set_title('Virtal Address by timeline')
+    axes[int(total_axes/2)].set_xlabel('timestamp')
+    axes[0].set_ylabel('Virtal Address')
 
-    axis.grid(True)
-    axis.set_title('Virtal Address by timeline')
-    axis.set_xlabel('timestamp')
-    axis.set_ylabel('Virtal Address')
 
     # output
     plt.savefig(dir_path+"/plot.png",format='png')
     if os.environ.get('DISPLAY','') != '':
     	plt.show()
-
